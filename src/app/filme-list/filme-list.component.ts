5805d5b792779c520b03a5f3741229386649a93d
Rafael Lucini <rafaellucini@gmail.com>
2019-11-20 13:37:12 -0300

    localStorage classes no template

--- src/app/filme-list/filme-list.component.ts
+++ src/app/filme-list/filme-list.component.ts
@@ -11,21 +11,65 @@ export class FilmeListComponent implements OnInit, OnDestroy {
 
   filmeList: Filme[];
 
-  constructor(private filmeService: FilmeService) { }
+  constructor(private filmeService: FilmeService) {
+  }
 
   ngOnInit(): void {
+    this.gerarFilmes();
+    // this.filmeService.remove(filme);
+    this.filmeList = this.filmeService.findAll();
+  }
+
+  private gerarFilmes(): void {
     const filme = new Filme();
     filme.id = 1;
     filme.titulo = 'Star Wars';
-    filme.precoBilhete = 40.5;
+    filme.precoBilhete = 10;
     filme.dataLancamento = new Date(1988, 3, 15);
     this.filmeService.add(filme);
-    // this.filmeService.remove(filme);
-    this.filmeList = this.filmeService.findAll();
+
+    const filme2 = new Filme();
+    filme2.id = 2;
+    filme2.titulo = 'Rambo';
+    filme2.precoBilhete = 15;
+    filme2.dataLancamento = new Date(1988, 3, 15);
+    filme2.inativo = true;
+    this.filmeService.add(filme2);
+
+    const filme3 = new Filme();
+    filme3.id = 3;
+    filme3.titulo = 'Clube da Luta';
+    filme3.precoBilhete = 30;
+    filme3.dataLancamento = new Date(1991, 3, 15);
+    this.filmeService.add(filme3);
+
+    const filme4 = new Filme();
+    filme4.id = 4;
+    filme4.titulo = 'Avengers';
+    filme4.precoBilhete = 50;
+    filme4.dataLancamento = new Date(1991, 3, 15);
+    this.filmeService.add(filme4);
   }
 
   ngOnDestroy(): void {
     this.filmeService.clear();
   }
 
+  getTitle(filme: Filme): string {
+    if (filme.precoBilhete <= 10) {
+      return 'Custo baixo';
+    }
+
+    if (filme.precoBilhete > 10 && filme.precoBilhete <= 20) {
+      return 'Custo regular';
+    }
+
+    if (filme.precoBilhete > 20 && filme.precoBilhete < 40) {
+      return 'Custo médio';
+    }
+
+    if (filme.precoBilhete >= 40) {
+      return 'Custo Alto';
+    }
+  }
 }
--- src/app/service/filme.service.ts
+++ src/app/service/filme.service.ts
@@ -7,26 +7,30 @@ import {Filme} from '../model/Filme';
 export class FilmeService {
 
   private filmeList: Filme[];
+  private KEY_FILMES = 'KEY_FILMES';
 
   constructor() {
     this.filmeList = [];
   }
 
   findAll(): Filme[] {
-    return this.filmeList;
+    return JSON.parse(localStorage.getItem(this.KEY_FILMES));
   }
 
   add(filme: Filme): void {
     this.filmeList.push(filme);
+    localStorage.setItem(this.KEY_FILMES, JSON.stringify(this.filmeList));
   }
 
   remove(filme: Filme): void {
     const index = this.filmeList.findIndex(item => item.id === filme.id);
 
     this.filmeList.splice(index, 1);
+    localStorage.setItem(this.KEY_FILMES, JSON.stringify(this.filmeList));
   }
 
   clear(): void {
     this.filmeList = [];
+    localStorage.removeItem(this.KEY_FILMES);
   }
 }
